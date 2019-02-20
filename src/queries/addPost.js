9adb7e89ab9639674244614f325914e547fd61e8
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-02-20 09:04:37 +0200

    add test

--- src/queries/addPost.js
+++ src/queries/addPost.js
@@ -1,11 +1,1 @@
-const dbConnection = require("../database/db_connection");
 
-const addPost = (postContent, user_key, cb) => {
-  const sql = `INSERT INTO posts (post_content,user_key) VALUES ($1,$2)`;
-  const values = [postContent, user_key];
-  dbConnection.query(sql, values, (err, res) => {
-    if (err) cb(err);
-    cb(null, "Successfully added");
-  });
-};
-module.exports = addPost;
